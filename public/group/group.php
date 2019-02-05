75a5d71560c7f842ae0a168448e3ede050948943
Luca Bösch <luca.boesch@bfh.ch>
2019-02-06 00:19:33 +0100

    MDL-64785 groups: page titles consistency.

--- public/group/group.php
+++ public/group/group.php
@@ -75,7 +75,7 @@ $context = context_course::instance($course->id);
 require_capability('moodle/course:managegroups', $context);
 
 $strgroups = get_string('groups');
-$PAGE->set_title($strgroups);
+$PAGE->set_title(get_string('groupscourse', 'group', $course->shortname));
 $PAGE->set_heading($course->fullname . ': '.$strgroups);
 $PAGE->set_pagelayout('admin');
 navigation_node::override_active_url(new moodle_url('/group/index.php', array('id' => $course->id)));
