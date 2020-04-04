df210f59f386dd6838aa0279b7c902fbde7db4d9
Luca Bösch <luca.boesch@bfh.ch>
2020-04-04 12:02:15 +0200

    MDL-60595 mod_lti: LTI tools name and description passing filters.

--- mod/lti/locallib.php
+++ mod/lti/locallib.php
@@ -1735,6 +1735,8 @@ function lti_get_tool_table($tools, $id) {
             $update = get_string('update', 'lti');
             $delete = get_string('delete', 'lti');
 
+            $type->name = format_string($type->name, true, array('context' => context_course::instance(SITEID)));
+
             if (empty($type->toolproxyid)) {
                 $baseurl = new \moodle_url('/mod/lti/typessettings.php', array(
                         'action' => 'accept',
@@ -2289,6 +2291,8 @@ function lti_filter_tool_types(array $tools, $state) {
 function lti_get_lti_types_by_course($courseid, $coursevisible = null) {
     global $DB, $SITE;
 
+    $returnedtypes = new stdClass();
+
     if ($coursevisible === null) {
         $coursevisible = [LTI_COURSEVISIBLE_PRECONFIGURED, LTI_COURSEVISIBLE_ACTIVITYCHOOSER];
     }
@@ -2305,14 +2309,22 @@ function lti_get_lti_types_by_course($courseid, $coursevisible = null) {
         return [];
     }
     $coursecond = implode(" OR ", $courseconds);
+    $returnedtypes = array();
     $query = "SELECT *
                 FROM {lti_types}
                WHERE coursevisible $coursevisiblesql
                  AND ($coursecond)
                  AND state = :active";
 
-    return $DB->get_records_sql($query,
+    $types = $DB->get_records_sql($query,
         array('siteid' => $SITE->id, 'courseid' => $courseid, 'active' => LTI_TOOL_STATE_CONFIGURED) + $coursevisparams);
+    foreach ($types as $type) {
+        // Entry to return.
+        $type->name = format_string($type->name, true, array('context' => context_course::instance($courseid)));
+        $type->description = format_string($type->description, true, array('context' => context_course::instance($courseid)));
+        $returnedtypes[] = $type;
+    }
+    return $returnedtypes;
 }
 
 /**
