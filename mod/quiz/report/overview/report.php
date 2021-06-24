bad4a90d994bd7cfc73886ee49f0a6d64cabee8b
Luca Bösch <luca.boesch@bfh.ch>
2021-06-24 22:05:32 +0200

    Align quiz attempts and override display.

--- mod/quiz/report/overview/report.php
+++ mod/quiz/report/overview/report.php
@@ -172,7 +172,6 @@ class quiz_overview_report extends attempts_report {
                     echo '</div>';
                     echo '</form>';
                     echo '</div>';
-                    echo '</div>';
                 }
                 // Print information on the grading method.
                 if ($strattempthighlight = quiz_report_highlighting_grading_method(
