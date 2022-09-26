44b6ff71380887ae4538e078b9e30c483501ac55
Nathan Nguyen <nathannguyen@catalyst-au.net>
2022-09-26 19:46:25 +1000

    MDL-72321 core_question: add hidden condition

--- lib/db/renamedclasses.php
+++ lib/db/renamedclasses.php
@@ -91,4 +91,5 @@ $renamedclasses = [
     'core_cohort\\local\\entities\\cohort_member' => 'core_cohort\\reportbuilder\\local\\entities\\cohort_member',
     'core_question\\bank\\search\\condition' => 'core_question\\local\\bank\\condition',
     'core_question\\bank\\search\\category_condition' => 'qbank_managecategories\\category_condition',
+    'core_question\\bank\\search\\hidden_condition' => 'qbank_deletequestion\\hidden_condition',
 ];
