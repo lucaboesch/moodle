lucaboesch/moodle-MDL-72752

MDL-71378 core: Changes to core_question and mod_quiz
This commit will change core_question and mod_quiz
to add mod_qbank activity as a supported plugin
as a thin layer around question bank.

Co-authored-by: Safat Shahin <safatshahin@catalyst-au.net>
Co-authored-by: Guillermo Gomez Arias <guillermogomez@catalyst-au.net>

mod/qbank/view.php
@@ -25,19 +25,26 @@
 
 require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/lib.php');
+require_once($CFG->dirroot . '/question/editlib.php');
 
 // Course module id.
 $id = optional_param('id', 0, PARAM_INT);
-
+$moduleid = optional_param('cmid', 0, PARAM_INT);
+$catparam = optional_param('cat', 0, PARAM_SEQUENCE);
+$categoryparam = optional_param('category', 0, PARAM_SEQUENCE);
 // Activity instance id.
 $q = optional_param('q', 0, PARAM_INT);
+
+if ($moduleid) {
+    $id = $moduleid;
+}
 
 if ($id) {
     $cm = get_coursemodule_from_id('qbank', $id, 0, false, MUST_EXIST);
     $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
     $moduleinstance = $DB->get_record('qbank', array('id' => $cm->instance), '*', MUST_EXIST);
 } else if ($q) {
-    $moduleinstance = $DB->get_record('qbank', array('id' => $n), '*', MUST_EXIST);
+    $moduleinstance = $DB->get_record('qbank', array('id' => $q), '*', MUST_EXIST);
     $course = $DB->get_record('course', array('id' => $moduleinstance->course), '*', MUST_EXIST);
     $cm = get_coursemodule_from_instance('qbank', $moduleinstance->id, $course->id, false, MUST_EXIST);
 } else {
@@ -45,9 +52,7 @@ if ($id) {
 }
 
 require_login($course, true, $cm);
-
 $modulecontext = context_module::instance($cm->id);
-
 require_capability('mod/qbank:view', $modulecontext);
 
 $event = \mod_qbank\event\course_module_viewed::create(array(
@@ -58,11 +63,37 @@ $event->add_record_snapshot('course', $course);
 $event->add_record_snapshot('qbank', $moduleinstance);
 $event->trigger();
 
-$PAGE->set_url('/mod/qbank/view.php', array('id' => $cm->id));
+list($thispageurl, $contexts, $cmid, $cm, $module, $pagevars) =
+        question_edit_setup('questions', '/mod/qbank/view.php', $cm->id);
+
+// Set question bank category param by default.
+if (!($catparam || $categoryparam)) {
+    $catid = $DB->get_field_select('question_categories', 'id', 'contextid = :contextid AND parent <> 0',
+        ['contextid' => $modulecontext->id]);
+    $cat = "{$catid},{$modulecontext->id}";
+    $pagevars['cat'] = $cat;
+}
+$PAGE->set_url(new moodle_url($thispageurl));
+
+$url = new moodle_url($thispageurl);
+// Qbank api call.
+$questionbank = new core_question\local\bank\view($contexts, $thispageurl, $course, $cm, $pagevars);
+
 $PAGE->set_title(format_string($moduleinstance->name));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_context($modulecontext);
 
+// Print horizontal nav if needed.
+$renderer = $PAGE->get_renderer('core_question', 'bank');
 echo $OUTPUT->header();
+
+
+// Render the selection action.
+$qbankaction = new \core_question\output\qbank_action_menu($url);
+echo $renderer->render($qbankaction);
+
+// Print the question area.
+$questionbank->component = 'mod_qbank';
+$questionbank->display();
 
 echo $OUTPUT->footer();

lucaboesch/moodle

mod/qbank/view.php
@@ -25,19 +25,26 @@
 
 require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/lib.php');
+require_once($CFG->dirroot . '/question/editlib.php');
 
 // Course module id.
 $id = optional_param('id', 0, PARAM_INT);
-
+$moduleid = optional_param('cmid', 0, PARAM_INT);
+$catparam = optional_param('cat', 0, PARAM_SEQUENCE);
+$categoryparam = optional_param('category', 0, PARAM_SEQUENCE);
 // Activity instance id.
 $q = optional_param('q', 0, PARAM_INT);
+
+if ($moduleid) {
+    $id = $moduleid;
+}
 
 if ($id) {
     $cm = get_coursemodule_from_id('qbank', $id, 0, false, MUST_EXIST);
     $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
     $moduleinstance = $DB->get_record('qbank', array('id' => $cm->instance), '*', MUST_EXIST);
 } else if ($q) {
-    $moduleinstance = $DB->get_record('qbank', array('id' => $n), '*', MUST_EXIST);
+    $moduleinstance = $DB->get_record('qbank', array('id' => $q), '*', MUST_EXIST);
     $course = $DB->get_record('course', array('id' => $moduleinstance->course), '*', MUST_EXIST);
     $cm = get_coursemodule_from_instance('qbank', $moduleinstance->id, $course->id, false, MUST_EXIST);
 } else {
@@ -45,9 +52,7 @@ if ($id) {
 }
 
 require_login($course, true, $cm);
-
 $modulecontext = context_module::instance($cm->id);
-
 require_capability('mod/qbank:view', $modulecontext);
 
 $event = \mod_qbank\event\course_module_viewed::create(array(
@@ -58,11 +63,38 @@ $event->add_record_snapshot('course', $course);
 $event->add_record_snapshot('qbank', $moduleinstance);
 $event->trigger();
 
-$PAGE->set_url('/mod/qbank/view.php', array('id' => $cm->id));
+list($thispageurl, $contexts, $cmid, $cm, $module, $pagevars) =
+        question_edit_setup('questions', '/mod/qbank/view.php', $cm->id);
+
+// Set question bank category param by default.
+if (!($catparam || $categoryparam)) {
+    $catid = $DB->get_fieldset_select('question_categories', 'id', 'contextid = :contextid AND parent <> 0',
+        ['contextid' => $modulecontext->id]);
+    $catid = reset($catid);
+    $cat = "{$catid},{$modulecontext->id}";
+    $pagevars['cat'] = $cat;
+}
+$PAGE->set_url(new moodle_url($thispageurl));
+
+$url = new moodle_url($thispageurl);
+// Qbank api call.
+$questionbank = new core_question\local\bank\view($contexts, $thispageurl, $course, $cm, $pagevars);
+
 $PAGE->set_title(format_string($moduleinstance->name));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_context($modulecontext);
 
+// Print horizontal nav if needed.
+$renderer = $PAGE->get_renderer('core_question', 'bank');
 echo $OUTPUT->header();
+
+
+// Render the selection action.
+$qbankaction = new \core_question\output\qbank_action_menu($url);
+echo $renderer->render($qbankaction);
+
+// Print the question area.
+$questionbank->component = 'mod_qbank';
+$questionbank->display();
 
 echo $OUTPUT->footer();