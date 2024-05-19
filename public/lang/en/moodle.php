576872b3706e605effb2bcfb21266d485e8dca2f
Luca Bösch <luca.boesch@bfh.ch>
2024-05-19 12:11:53 +0200

    MDL-79193 core_auth: Notify about suspended account on login page.

--- public/lang/en/moodle.php
+++ public/lang/en/moodle.php
@@ -2195,6 +2195,7 @@ $string['success'] = 'Success';
 $string['successduration'] = 'Success ({$a} seconds)';
 $string['summary'] = 'Summary';
 $string['suspended'] = 'Suspended';
+$string['suspendedlogin'] = 'Your account has been suspended. Please contact the site administrator.';
 $string['suspendedusers'] = 'Suspended users';
 $string['switchdevicedefault'] = 'Switch to the standard theme';
 $string['switchdevicerecommended'] = 'Switch to the recommended theme for your device';
