846de83024508fd54ef50be4add800ca48aa1c9a
Luca Bösch <luca.boesch@bfh.ch>
2025-01-24 12:49:06 +0100

    MDL-83985 mod_bigbluebuttonbn: change name to 1333 chars

--- mod/bigbluebuttonbn/db/upgrade.php
+++ mod/bigbluebuttonbn/db/upgrade.php
@@ -65,6 +65,20 @@ function xmldb_bigbluebuttonbn_upgrade($oldversion = 0) {
     // Automatically generated Moodle v4.5.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2024121801) {
+
+        // Changing precision of field name on table bigbluebuttonbn to (1333).
+
+        $table = new xmldb_table('bigbluebuttonbn');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'intro');
+
+        // Launch change of precision for field fullname.
+        $dbman->change_field_precision($table, $field);
+
+        // Data savepoint reached.
+        upgrade_mod_savepoint(true, 2024121801, 'bigbluebuttonbn');
+    }
+
     return true;
 }
 
