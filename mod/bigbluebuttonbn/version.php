6ccc3cab0b35c61cc68b2587d6b0c10755623484
Luca Bösch <luca.boesch@bfh.ch>
2025-01-24 12:49:06 +0100

    MDL-83985 mod_bigbluebuttonbn: change name to 1333 chars

--- mod/bigbluebuttonbn/version.php
+++ mod/bigbluebuttonbn/version.php
@@ -27,6 +27,6 @@
 defined('MOODLE_INTERNAL') || die;
 
 
-$plugin->version = 2025011000;
+$plugin->version = 2025011101;
 $plugin->requires = 2024100100;
 $plugin->component = 'mod_bigbluebuttonbn';
