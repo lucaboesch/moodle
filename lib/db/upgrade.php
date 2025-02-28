5751d10eac63b6ed3435db4e72d19f28fcf09798
Luca Bösch <luca.boesch@bfh.ch>
2025-02-28 10:51:58 +0100

    MDL-83985 badges: change name and issuername to 1333 chars

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -1464,5 +1464,27 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2025022100.02);
     }
 
+    if ($oldversion < 2025013100.03) {
+
+        // Changing precision of field name on table badge to (1333).
+
+        $table = new xmldb_table('badge');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'id');
+
+        // Launch change of precision for field name.
+        $dbman->change_field_precision($table, $field);
+
+        // Changing precision of field issuername on table badge to (1333).
+
+        $table = new xmldb_table('badge');
+        $field = new xmldb_field('issuername', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'usermodified');
+
+        // Launch change of precision for field issuername.
+        $dbman->change_field_precision($table, $field);
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2025013100.03);
+    }
+
     return true;
 }
